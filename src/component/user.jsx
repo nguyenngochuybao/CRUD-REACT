7b46338d01aa854015b3ec4b12ad810d01b8bbe9
nguyenngochuybao <baokx2k3@gmail.com>
2024-09-02 21:44:41 +0700

    up.

--- src/component/user.jsx
+++ src/component/user.jsx
@@ -1,15 +1,38 @@
+import axios from 'axios';
 import './style.css'
+import { useEffect, useState } from 'react';
 
 function User ()
 {
+    const [ data, setData ] = useState( [] )
+
+    useEffect( () =>
+    {
+        axios.get( "http://localhost:3000/users" )
+            .then( res => setData( res.data ) )
+            .catch( error => ( error ) );
+
+    }, [] )
+
+
+
     return (
         <div className="user">
             <div className='container'>
-                <ul>
-                    <li className='li_id'>id</li>
-                    <li className='li_name'>name</li>
-                    <li className='li_email'>email</li>
+                <ul className='user_ul'>
+                    <li className='li1'>id</li>
+                    <li className='li2'>name</li>
+                    <li className='li3'>email</li>
                 </ul>
+                {
+                    data.map( ( data, index ) =>
+                    ( <ul className='user_ul' key={ index }>
+                        <li className='li1'>{ data.id }</li>
+                        <li className='li2'>{ data.name }</li>
+                        <li className='li3'>{ data.email }</li>
+                    </ul> )
+                    )
+                }
             </div>
         </div>
     );
